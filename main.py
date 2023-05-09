6f298f581e6d44993e756615b8d546c7d635bafb
lysergife <dolgaialubov05@gmail.com>
2023-05-09 17:31:49 +0300

    player movements are implemented

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 # Import and initialize the pygame library
 import pygame
 pygame.init()
+screen_width = 800
+screen_height = 500
+box_width = 100
+box_height = 100
+player_x = 0
+player_y = 0
+vel_x = 0.2
+vel_y = 0.2
 
 # Set up the drawing window
-screen = pygame.display.set_mode([500, 500])
+screen = pygame.display.set_mode([screen_width, screen_height])
 
 # Run until the user asks to quit
 running = True
@@ -14,14 +22,25 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # Fill the background with white
-    screen.fill((255, 255, 255))
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_d]:
+        player_x += vel_x
+    if keys[pygame.K_a]:
+        player_x -= vel_x
+    if keys[pygame.K_w]:
+        player_y -= vel_y
+    if keys[pygame.K_s]:
+        player_y += vel_y
 
-    # Draw a solid blue circle in the center
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+    screen.fill((255, 255, 255))
+    pygame.draw.rect(screen, (0, 0, 255), (player_x - box_width / 2, player_y - box_height / 2,
+                                           box_width, box_height))
+    if keys[pygame.K_SPACE]:
+        pygame.draw.circle(screen, (44, 55, 66), (player_x, player_y), 10, 0)
 
     # Flip the display
     pygame.display.flip()
 
 # Done! Time to quit.
 pygame.quit()
+
